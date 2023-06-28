54f3ab04e2b9e7f6b21d8d857d9e97bdba6cace0
Luiz Gustavo Francisco <luiz.francisco@docente.pr.senac.br>
2023-06-28 12:05:49 -0300

    lista edição e exclusao

--- senac/src/main/java/com/sistema/senac/ConfiguracaoBancoDeDados.java
+++ senac/src/main/java/com/sistema/senac/ConfiguracaoBancoDeDados.java
@@ -15,7 +15,7 @@ public class ConfiguracaoBancoDeDados {
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("org.postgresql.Driver"); //declara as configurações de acesso
-		dataSource.setUrl("jdbc:postgresql://localhost:5432/loja");
+		dataSource.setUrl("jdbc:postgresql://localhost:5432/banconovatur");
 		dataSource.setUsername("postgres");//usuario
 		dataSource.setPassword("postgres");//senha
 		return dataSource;
